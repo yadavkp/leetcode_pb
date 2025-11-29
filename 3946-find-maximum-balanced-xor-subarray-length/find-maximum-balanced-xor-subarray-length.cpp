6caa33bff8103d19bca20c4eebe7186cab9c4f68
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-11-29 21:57:13 +0530

    Time: 772 ms (39.43%) | Memory: 350.9 MB (62.47%) - LeetSync

--- 3946-find-maximum-balanced-xor-subarray-length/find-maximum-balanced-xor-subarray-length.cpp
+++ 3946-find-maximum-balanced-xor-subarray-length/find-maximum-balanced-xor-subarray-length.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int maxBalancedSubarray(vector<int>& a) {
-        int mx = 0;int n = a.size();
-         map<pair<int,int>,int> mp;
-        int ev = 0,od=0,xr=0;
+        int n= a.size(),mx = 0,xr = 0,ev=0,od=0;
+
+        map<pair<int,int>,int> mp;
+        
         mp[{0,0}] = -1;
         for(int i=0;i<n;i++){
             xr ^= a[i];
             if(a[i]%2)od++;
             else ev++;
-            
             int diff = od - ev;
-            pair<int,int> key = {xr,diff};
+            pair<int,int> key = {xr,diff };
             if(mp.count(key)){
-                mx = max(mx,i - mp[key]);
+                mx = max(mx, i - mp[key]);
             }else{
                 mp[key] = i;
             }
-           
         }
         return mx;
     }
