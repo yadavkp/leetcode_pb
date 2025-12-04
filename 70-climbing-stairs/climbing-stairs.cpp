a92367e8dede6104de23a77909c34e6d4d57f24b
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-04 11:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,28 +1,59 @@
+// class Solution {
+    
+//     int  solve(int n,vector<int>&dp){
+
+//         if(n < 0 ){
+//             return 0;
+//         }
+      
+//         if(n == 0){
+//             return  dp[n] + 1;
+//         }
+
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+//         dp[n] = solve(n-1,dp)  + solve(n-2,dp);
+//         return dp[n];
+
+//     }
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(50,-1);
+//         dp[0] = 0;
+//         // int ans = solve(n,dp);
+//         return solve(n,dp);
+//     }
+// };
+
+
+// by bottom to up  appraoch -> 
+
 class Solution {
-    vector<int> dp;
+    int  solve(int n,int i,vector<int>&dp){
 
-    int  solve(int n,vector<int>&dp){
-
-        if(n < 0 ){
+        if(i > n ){
             return 0;
         }
       
-        if(n == 0){
-            return  1;
+        if(i == n){
+            return   1;
         }
-        
-        if(dp[n] != -1){
-            return dp[n];
+
+        if(dp[i] != -1){
+            return dp[i];
         }
-        dp[n] = solve(n-1,dp)  + solve(n-2,dp);
-        return dp[n];
+
+        dp[i] = solve(n,i+1,dp)  + solve(n,i+2,dp);
+        return dp[i];
 
     }
 public:
     int climbStairs(int n) {
         vector<int> dp(50,-1);
-        dp[0] = 0;
-        int ans = solve(n,dp);
-        return ans;
+
+        //dp[0] = 0;
+        
+        return solve(n,0,dp);
     }
 };
