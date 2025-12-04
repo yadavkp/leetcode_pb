473d47abc5f39a816c6dfb337a49f64c83827042
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-04 10:17:00 +0530

    Time: 2 ms (76.50%) | Memory: 7.9 MB (37.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -28,16 +28,34 @@
 
 // ->>>>>>>>>>>>>>>>>>>>>>> bottom up approach ->
 
+// class Solution {
+ 
+// public:
+   
+//     int fib(int n) {
+//       vector<int> dp(40,-1);
+//       dp[0] = 0,dp[1] = 1;
+//      for(int i=2;i<=n;i++){
+//         dp[i] = dp[i-1] + dp[i-2];
+//      }
+//       return dp[n]; 
+//     }
+// };
+
+
 class Solution {
  
 public:
    
     int fib(int n) {
-      vector<int> dp(40,-1);
-      dp[0] = 0,dp[1] = 1;
-     for(int i=2;i<=n;i++){
-        dp[i] = dp[i-1] + dp[i-2];
-     }
-      return dp[n]; 
+        int ans = 0,p1=1,p2 = 0;
+        if(n <= 1)return n;
+
+        for(int i=2;i<=n;i++){
+            ans = p1 + p2;
+            p2 = p1;
+            p1 = ans;
+        }
+        return ans;
     }
 };
