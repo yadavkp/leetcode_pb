02f1312eb001b1f96e4b99dda2dedbf5325d2093
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-04 10:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (21.90%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,43 @@
 
-class Solution {
-     vector<int> dp = vector<int>(40,-1);
+// // inside a class you can't initialize directly initializer
+// class Solution {
+//      //vector<int> dp = vector<int>(40,-1);
+//      // this is another method intialize and passing parameter and make constructor
+//       vector<int> dp; // ->  that means u make a dp funciton that store two parameter
+//      // // now call constructor
+    
        
-public:
-    int fib(int n) {
-      dp[0] = 0;
-      dp[1] = 1;
+// public:
+//     Solution()  : dp(40,-1){
+//         dp[0] = 0;
+//         dp[1] = 1;
+//      }
+//     int fib(int n) {
       
-      if(n <= 1){
-        return n;
-      }
-      if(dp[n] != -1){
-        return dp[n];
-      }
-      dp[n] =  fib(n-1) + fib(n-2);
+//       if(n <= 1){
+//         return n;
+//       }
+//       if(dp[n] != -1){
+//         return dp[n];
+//       }
+//       dp[n] =  fib(n-1) + fib(n-2);
+//       return dp[n]; 
+//     }
+// };
+
+
+// ->>>>>>>>>>>>>>>>>>>>>>> bottom up approach ->
+
+class Solution {
+ 
+public:
+   
+    int fib(int n) {
+      vector<int> dp(40,-1);
+      dp[0] = 0,dp[1] = 1;
+     for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+     }
       return dp[n]; 
     }
 };
