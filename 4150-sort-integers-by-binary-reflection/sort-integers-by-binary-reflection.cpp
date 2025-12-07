337f9ce0bbac3362abdc8c352981b601a5db6df6
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-07 15:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (96.56%) - LeetSync

--- 4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.cpp
+++ 4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    vector<int> sortByReflection(vector<int>& nums) {
-        vector<pair<int,int>> ans;
-        for(int i=0;i<nums.size();i++){
+    vector<int> sortByReflection(vector<int>& a) {
+        
+        auto getbinreflection = [&](int a)-> int
+        {
             int rev = 0;
-            int num = nums[i];
-            int p = num;
-            while(num > 0){
-               rev = ((rev << 1) | (num&1) );
-               num >>= 1;
+            while(a > 0){
+                rev = (rev << 1 | (a&1));
+                a >>= 1;
             }
-           
-           
-            ans.push_back({rev,p});
-            
-        }
-        sort(ans.begin(),ans.end(),[&](const auto &a, const auto &b){
-            if(a.first == b.first){
-                return a.second < b.second ;
-            } 
-            return a.first < b.first;
-        });
-        vector<int> a;
-        for(auto t : ans){
-            a.push_back(t.second);
-        }
-
+            return rev;
+        };
+        auto cmp = [&](int a,int b) -> bool
+        {
+            int bin1 = getbinreflection(a);
+            int bin2 = getbinreflection(b);
+            if(bin1 == bin2) return a < b;
+            return bin1 < bin2;
+        };
+        sort(a.begin(),a.end(),cmp);
         return a;
     }
 };
