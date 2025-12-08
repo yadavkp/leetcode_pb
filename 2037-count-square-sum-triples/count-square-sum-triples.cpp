62317e33ed30e6b00ff6b6af7f269d185704163e
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-08 19:37:51 +0530

    Time: 7 ms (62.17%) | Memory: 7.7 MB (98.67%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     int countTriples(int n) {
         
-        auto square= [&](int n)-> bool
-        {
-            int num = (int)sqrt(n);
-           
-            return (num*num == n);
-           
-            
-
-        };
-
         int cnt = 0;
         for(int i=1;i<=n;i++){
             for(int j = 1;j<=n;j++){
                 int sum = i*i + j*j;
-                if(square((sum))){
-                    int c = (int)sqrt(sum);
-                    if(c <= n){
-                        cnt++;
-                    }
+                int c = (int)sqrt(sum);
+                if(c <= n && c*c == sum){
+                    cnt++;
                 }
+               
             }
         }
         return cnt;
