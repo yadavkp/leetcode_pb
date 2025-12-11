4712784e18844b9bbbeb418e65c8df8ff99650dd
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-11 10:16:06 +0530

    Time: 1 ms (39.82%) | Memory: 16.3 MB (36.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -31,30 +31,56 @@
 // };
 
 
-class Solution {
-    vector<vector<int>> dp; 
+// class Solution {
+//     vector<vector<int>> dp;
+//     int solve(int r, int c, vector<vector<int>>& grid) {
+//         int m = grid.size(), n = grid[0].size();
+        
+//         if (r >= m || c >= n) return INT_MAX;
     
-    int solve(int r, int c, vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+//         if (r == m-1 && c == n-1) return grid[r][c];
         
-        if (r >= m || c >= n) return INT_MAX;
-    
-        if (r == m-1 && c == n-1) return grid[r][c];
+//         if (dp[r][c] != -1) return dp[r][c];
         
-        if (dp[r][c] != -1) return dp[r][c];
-        
-        int right = solve(r, c+1, grid);
-        int down  = solve(r+1, c, grid);
+//         int right = solve(r, c+1, grid);
+//         int down  = solve(r+1, c, grid);
         
        
-        dp[r][c] = grid[r][c] + min(right, down);
-        return dp[r][c];
-    }
+//         dp[r][c] = grid[r][c] + min(right, down);
+//         return dp[r][c];
+//     }
     
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size(), n = grid[0].size();
+//         dp.assign(m, vector<int>(n, -1));
+//         return solve(0, 0, grid);
+//     }
+// };
+
+
+class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        dp.assign(m, vector<int>(n, -1));
-        return solve(0, 0, grid);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int r = 0;r < m;r++){
+            for(int c = 0;c < n;c++){
+                int sum = INT_MAX;
+                if(r-1 >= 0){
+                    //dp[r-1][c] = min()
+                    sum  = min(sum,dp[r-1][c]);
+                }
+
+                if(c-1 >= 0){
+                    sum = min(sum,dp[r][c-1]);
+                }
+
+                dp[r][c] = (sum == INT_MAX ? 0 : sum) + grid[r][c];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
