809b5300983e95543259577ba94fed99e7486595
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-12 20:58:14 +0530

    Time: 2 ms (73.83%) | Memory: 96.2 MB (35.33%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int n = nums.size();
+        if(n==2){
+            return 2;
+        }
+        int ev = 0,od = 0;
+        for(auto t  :nums){
+            if(t%2)od++;
+            else ev++;
+        }
+        if(od==0)return ev;
+        else if(ev == 0)return od;
+
+        int mx = 1;
+        int turn = (nums[0]%2==0 ? 0 : 1);
+        turn = !turn;
+        for(int i=0;i<n;i++){
+            if(turn == nums[i]%2){
+                mx++;
+                turn = !turn;
+            }
+        }
+        return max({od,ev,mx});
+    }
+};
