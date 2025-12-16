e9d13e4c12fd2acb74269564ac8316e2b9e2fe5a
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-16 15:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (27.16%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -23,29 +23,53 @@
 
 // 
 
-// Memoisaton 
+// // Memoisaton 
+// class Solution {
+//     vector<vector<int>> dp;
+//     int path(int r, int c){
+
+//         // b case 
+//         if(r == 0 && c == 0){
+//             return 1;
+//         }
+        
+//         if(r < 0 || c < 0 )return 0;
+
+//         // checking 
+//         if(dp[r][c]  != -1)return dp[r][c];
+
+//         int left = path(r,c-1);
+//         int up = path(r-1,c);
+        
+//         return dp[r][c] = left + up;
+//     }
+// public:
+//     int uniquePaths(int m, int n) {
+//         dp.assign(m,vector<int>(n,-1));
+//         return path(m-1,n-1);
+//     }
+// };
+
+
+
+// tabulation ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 class Solution {
-    vector<vector<int>> dp;
-    int path(int r, int c){
-
-        // b case 
-        if(r == 0 && c == 0){
-            return 1;
-        }
-        
-        if(r < 0 || c < 0 )return 0;
-
-        // checking 
-        if(dp[r][c]  != -1)return dp[r][c];
-
-        int left = path(r,c-1);
-        int up = path(r-1,c);
-        
-        return dp[r][c] = left + up;
-    }
 public:
     int uniquePaths(int m, int n) {
+        vector<vector<int>> dp;
         dp.assign(m,vector<int>(n,-1));
-        return path(m-1,n-1);
+        for(int i=0;i<m;i++){
+            for(int j = 0; j< n;j++){
+                if(i == 0 && j == 0)dp[i][j] = 1;
+                else{
+                    int left=0,up = 0;
+                    if(i>0) up += dp[i-1][j];  // up function 
+                    if(j>0) left += dp[i][j-1]; // left function 
+                    dp[i][j] = left + up; // sum of both count path
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
