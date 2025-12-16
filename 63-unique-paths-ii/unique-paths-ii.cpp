7d68199c054818864888972a34c41722836ddfa4
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-16 18:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (82.97%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -50,26 +50,53 @@
 /// ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
+// class Solution {
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+//         int n = Grid.size(), m = Grid[0].size();
+//         vector<vector<int>> dp;
+//         dp.assign(n,vector<int>(m,0));
+//         for(int i=0;i<n;i++){
+//             for(int j = 0; j <  m ; j++){
+//                 if(i == 0 && j == 0 && Grid[i][j] == 0)dp[i][j] = 1;
+//                 else{
+//                     int top = 0,left= 0;
+//                     if(i > 0) top += dp[i-1][j];
+//                     if(j > 0) left += dp[i][j-1];
+//                     if(Grid[i][j] != 1){
+//                         dp[i][j] = top + left;
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[n-1][m-1];
+//     }
+// };
+
+
+//->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// space optmisation 
+
 class Solution {
-    
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int n = Grid.size(), m = Grid[0].size();
-        vector<vector<int>> dp;
-        dp.assign(n,vector<int>(m,0));
+        vector<int>prev(m,0); // no of cells , whwere we store data
         for(int i=0;i<n;i++){
+            vector<int> curr(m,0);
             for(int j = 0; j <  m ; j++){
-                if(i == 0 && j == 0 && Grid[i][j] == 0)dp[i][j] = 1;
+                if(i == 0 && j == 0 && Grid[i][j] == 0)curr[j] = 1;
                 else{
                     int top = 0,left= 0;
-                    if(i > 0) top += dp[i-1][j];
-                    if(j > 0) left += dp[i][j-1];
+                    if(i > 0) top += prev[j];
+                    if(j > 0) left += curr[j-1];
                     if(Grid[i][j] != 1){
-                        dp[i][j] = top + left;
+                        curr[j] = top + left;
                     }
                 }
             }
+            prev = curr;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
