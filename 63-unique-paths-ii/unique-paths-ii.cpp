4eac9e871d811dbb61b203a2b76b8a25708759e9
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-16 18:37:06 +0530

    Time: 1 ms (16.15%) | Memory: 11.9 MB (26.00%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -21,26 +21,55 @@
 
 
 // memoisation  ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// class Solution {
+//     vector<vector<int>> dp;
+//     int path(int r,int c,vector<vector<int>>& grid){
+//         if(r == 0 && c == 0 && grid[r][c]==0)return 1;
+
+//         if(r < 0 || c < 0 || grid[r][c] == 1)return 0;
+
+//         // checking krbake jao 
+//         if(dp[r][c] != -1){
+//             return dp[r][c];
+//         }
+//         int left = path(r,c-1,grid);
+//         int up = path(r-1,c,grid);
+
+//         return    dp[r][c] =  left + up;
+//     }
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+//         int n = Grid.size(), m = Grid[0].size();
+//         dp.assign(n,vector<int>(m,-1));
+//         return path(n-1,m-1,Grid);
+//     }
+// };
+
+
+//tabulation  ->>>>>>>>>>>>>>>>
+/// ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 class Solution {
-    vector<vector<int>> dp;
-    int path(int r,int c,vector<vector<int>>& grid){
-        if(r == 0 && c == 0 && grid[r][c]==0)return 1;
-
-        if(r < 0 || c < 0 || grid[r][c] == 1)return 0;
-
-        // checking krbake jao 
-        if(dp[r][c] != -1){
-            return dp[r][c];
-        }
-        int left = path(r,c-1,grid);
-        int up = path(r-1,c,grid);
-
-        return    dp[r][c] =  left + up;
-    }
+    
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int n = Grid.size(), m = Grid[0].size();
-        dp.assign(n,vector<int>(m,-1));
-        return path(n-1,m-1,Grid);
+        vector<vector<int>> dp;
+        dp.assign(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j = 0; j <  m ; j++){
+                if(i == 0 && j == 0 && Grid[i][j] == 0)dp[i][j] = 1;
+                else{
+                    int top = 0,left= 0;
+                    if(i > 0) top += dp[i-1][j];
+                    if(j > 0) left += dp[i][j-1];
+                    if(Grid[i][j] != 1){
+                        dp[i][j] = top + left;
+                    }
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
