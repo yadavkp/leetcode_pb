21c71a355bf899e31b33822c4ef28af9c62429bc
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-26 14:57:39 +0530

    Time: 113 ms (22.58%) | Memory: 89.5 MB (9.70%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -1,35 +1,65 @@
-// recursion  ->>>>>>>>>>>>>>>>>>>>
+// recursion  ->>>>>>>>>>>>>>>>>>>> + memoisation
+// class Solution {
+//     int mx = 0,n1,n2,cnt = 0;
+//     vector<vector<int>> dp;
+//     int solve(int i,int j,vector<int>&a1,vector<int>&a2){
+
+//         if( i>= n1 || j >= n2){
+//             return 0;
+//         }
+
+//         int match = 0;
+//         if(dp[i][j] != -1)return dp[i][j];
+//         if(a1[i] == a2[j]){
+          
+//             match = 1 + solve(i+1,j+1,a1,a2);
+//         }else{
+//             return 0;
+//         }
+        
+//        return dp[i][j] = match;
+        
+//     }
+
+// public:
+//     int findLength(vector<int>& a1, vector<int>& a2) {
+//         n1 = a1.size(),n2 = a2.size();
+//         dp.assign(n1,vector<int>(n2,-1));
+//         for(int i=0;i<n1;i++){
+//             for(int j = 0;j < n2;j++){
+                
+//                 mx = max(mx,solve(i,j,a1,a2));
+               
+//             }
+//         }
+         
+//         return mx;
+//     }
+// };
+
+
+
 class Solution {
     int mx = 0,n1,n2,cnt = 0;
     vector<vector<int>> dp;
-    int solve(int i,int j,vector<int>&a1,vector<int>&a2){
-
-        if( i>= n1 || j >= n2){
-            return 0;
-        }
-
-        int match = 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        if(a1[i] == a2[j]){
-          
-            match = 1 + solve(i+1,j+1,a1,a2);
-        }else{
-            return 0;
-        }
-        
-       return dp[i][j] = match;
-        
-    }
-
 public:
     int findLength(vector<int>& a1, vector<int>& a2) {
         n1 = a1.size(),n2 = a2.size();
-        dp.assign(n1,vector<int>(n2,-1));
-        for(int i=0;i<n1;i++){
-            for(int j = 0;j < n2;j++){
+        dp.assign(n1+1,vector<int>(n2+1,0));
+
+    // base case  // but don't need to fill that
+        for(int i=0;i<=n1;i++)dp[i][n2] = 0;
+        for(int j = 0;j <= n2;j++)dp[n2][j] = 0;
+
+// manually fill the dp table  _>>>>>>>>>>, then come up the soln
+        for(int i=n1-1;i>=0;i--){
+            for(int j = n2-1;j >=0;j--){
                 
-                mx = max(mx,solve(i,j,a1,a2));
-               
+                if(a1[i] == a2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];  //access the data next diag cell+1
+                }
+                mx= max(mx,dp[i][j]);
+              
             }
         }
          
@@ -67,3 +97,7 @@ public:
 //         return mx;
 //     }
 // };
+
+
+
+
