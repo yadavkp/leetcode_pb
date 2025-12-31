d509bc89e8dd0f40834915f044da9d64996fd265
yadavkp <163226725+yadavkp@users.noreply.github.com>
2025-12-31 15:03:53 +0530

    Time: 54 ms (3.70%) | Memory: 12.4 MB (99.33%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,39 +1,78 @@
+// class Solution {
+
+//     int solve(int c_r,int idx,vector<vector<int>>& triangle){
+//         int n = triangle.size();
+
+//         if(c_r == n-1){
+//             return triangle[c_r][idx];
+//         }
+
+//         int sum  = triangle[c_r][idx];
+//         int left = solve(c_r+1,idx,triangle);
+//         int right = solve(c_r+1,idx+1,triangle);
+
+//         return sum + min(left,right);
+//     }
+// public:
+
+//     int minimumTotal(vector<vector<int>>& triangle) {
+        
+//         int n = triangle.size();
+//         return solve(0,0,triangle);
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+        
+//         int n = triangle.size();
+
+//         vector<vector<int>> dp(n,vector<int>(n,0));
+//         for(int j = 0;j<n;j++){
+//             dp[n-1][j] = triangle[n-1][j];
+//         }
+//         for(int r = n-2;r >=0;r--){
+//             int m = triangle[r].size(); // current column size 
+//             for(int c = 0;c < m;c++){
+//                 int left = dp[r+1][c];
+//                 int right = dp[r+1][c+1];
+//                 dp[r][c] = triangle[r][c] +min(left,right);
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// };
+
+
+
 class Solution {
-
-    int solve(int c_r,int idx,vector<vector<int>>& triangle){
-
-        int n = triangle.size();
-
-        if(c_r == n-1){
-            return triangle[c_r][idx];
-        }
-
-        int sum  = triangle[c_r][idx];
-        int left = solve(c_r+1,idx,triangle);
-        int right = solve(c_r+1,idx+1,triangle);
-
-        return sum + min(left,right);
-    }
-
 public:
-
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n = triangle.size();
 
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int j = 0;j<n;j++){
-            dp[n-1][j] = triangle[n-1][j];
-        }
-        for(int r = n-2;r >=0;r--){
-            int m = triangle[r].size(); // current column size 
-            for(int c = 0;c < m;c++){
-                int left = dp[r+1][c];
-                int right = dp[r+1][c+1];
-                dp[r][c] = triangle[r][c] +min(left,right);
+        int mn = 1e9;
+        for(int i=1;i<n;i++){
+            for(int j = 0;j <= i;j++){
+                if(j == 0)triangle[i][j] += triangle[i-1][j];
+                else if(j == i)triangle[i][j] += triangle[i-1][j-1];
+                else{
+                    triangle[i][j] += min(triangle[i-1][j],triangle[i-1][j-1]);
+                }
+                cout<<triangle[i][j]<<" ";
+                
             }
+            cout<<"\n";
         }
 
-        return dp[0][0];
+        for(int i=0;i<n;i++){
+            mn = min(mn,triangle[n-1][i]);
+        }
+        return mn;
+        //return dp[0][0];
     }
 };
