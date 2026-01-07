bc843b16c9d432d78d4d3304ea80c17be849ab03
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-01-07 09:57:31 +0530

    Time: 3 ms (31.17%) | Memory: 21.6 MB (15.82%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+        
+//         int pft = 0,prev_stock = 0;
+//         prev_stock = prices[0];
+
+//         for(int i=1;i<prices.size();i++){
+//             pft += (prices[i]>=prev_stock ? prices[i]-prev_stock:0);
+//             prev_stock = prices[i];
+//         }
+//         return pft;
+//     }
+// };
+
+
+// flag ==0,  neutral
+// flag = 1 ,to ab sell krna hi hai
+
+vector<vector<int>> dp;
+
+class Solution {
+
+    int solve(int i,int flag,vector<int>&price){
+
+        if(i >= price.size())return 0;
+
+        if(dp[i][flag] != -1)return dp[i][flag];
+        int buy = 0,sell = 0;
+        if(flag ==0){
+            int take = - price[i] + solve(i+1,1,price);
+            int not_take = 0 + solve(i+1,0,price);
+            buy =  max(take,not_take);
+
+        }
+        if(flag==1){
+            int take = price[i] + solve(i+1,0,price);
+            int not_take = solve(i+1,1,price);
+            sell =  max(take,not_take);
+        }
+
+        return dp[i][flag] =  max(buy,sell);
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        dp.assign(n,vector<int>(2,-1));
+       int profit = solve(0,0,prices);
+
+       return profit;
+    }
+};
