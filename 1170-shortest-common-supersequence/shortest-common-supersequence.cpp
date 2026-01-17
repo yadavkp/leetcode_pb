b99f5b36ad41220565f11b0f24b68a5d6b30b841
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-01-17 11:09:10 +0530

    Time: 27 ms (38.47%) | Memory: 26.8 MB (71.08%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,44 +1,6 @@
-// class Solution {
-//     int n1,n2;
-//     vector<vector<string>> dp;
 
-
-//     string f(int i,int j, string &s1,string &s2){
-
-//         if(i == n1) return s2.substr(j,n2-j);
-
-//         if(j == n2) return s1.substr(i,n1-i);
-
-//         if(dp[i][j] != "")return dp[i][j];
-
-//         if(s1[i] == s2[j]){
-
-//             return dp[i][j] =  s1[i]  + f(i+1,j+1,s1,s2);
-
-//         }else{
-
-//             string first = s1[i] + f(i+1,j,s1,s2);
-//             string second = s2[j] + f(i,j+1,s1,s2);
-
-//             return dp[i][j] =  (first.size() <= second.size() ? first : second);
-//         }
-
-//     }
-
-// public:
-//     string shortestCommonSupersequence(string s1, string s2) {
-        
-//         n1 = s1.size(),n2 = s2.size();
-//         dp.assign(n1,vector<string>(n2,"") );
-//         string ans = f(0,0,s1,s2);
-//         //cout<<ans.size();
-//         return ans;
-//     }
-// };
-
-
-
-
+// this method, first find lcs, then add remaining char in strnig ans, then reverse
+// 2nd method was-> if u find the len -> m + n - lcs;
 class Solution {
     int n1,n2;
     vector<vector<int>> dp;
@@ -86,3 +48,47 @@ public:
           
     }
 };
+
+
+
+// thsi code would give mle, so we have not learn  that
+
+
+
+// class Solution {
+//     int n1,n2;
+//     vector<vector<string>> dp;
+
+
+//     string f(int i,int j, string &s1,string &s2){
+
+//         if(i == n1) return s2.substr(j,n2-j);
+
+//         if(j == n2) return s1.substr(i,n1-i);
+
+//         if(dp[i][j] != "")return dp[i][j];
+
+//         if(s1[i] == s2[j]){
+
+//             return dp[i][j] =  s1[i]  + f(i+1,j+1,s1,s2);
+
+//         }else{
+
+//             string first = s1[i] + f(i+1,j,s1,s2);
+//             string second = s2[j] + f(i,j+1,s1,s2);
+
+//             return dp[i][j] =  (first.size() <= second.size() ? first : second);
+//         }
+
+//     }
+
+// public:
+//     string shortestCommonSupersequence(string s1, string s2) {
+        
+//         n1 = s1.size(),n2 = s2.size();
+//         dp.assign(n1,vector<string>(n2,"") );
+//         string ans = f(0,0,s1,s2);
+//         //cout<<ans.size();
+//         return ans;
+//     }
+// };
