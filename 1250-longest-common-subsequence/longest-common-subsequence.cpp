712799d4168eed43e1d6b00fcecef629106db27a
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-01-17 10:12:01 +0530

    Time: 46 ms (13.92%) | Memory: 27.7 MB (15.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,3 @@
-/* we solve this pb by 
-idea->  start looking from thr back in memoization
-        start looking from the start in bottom up approach
-
-bottom up approach that is very efficient for this,where we store value if subsequence occuring matches
-and return posn of dp[n][m]
-
-if you want to print the subsequence -> start traversing from back and char match
-store them 
-
-*/
-
-// class Solution {
-// public:
-//     int longestCommonSubsequence(string text1, string text2) {
-        
-//         int n = text1.size(),m = text2.size();
-//         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-
-//         for(int i=1;i<=n;i++){
-//             for(int j = 1;j<=m;j++){
-//                 if(text1[i-1] == text2[j-1]){
-//                     dp[i][j] = 1+ dp[i-1][j-1];  // diagonally store
-//                 }else{
-//                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-//                 }
-//             }
-//         }
-//         return dp[n][m];
-//     }
-// };
-
 
 // recursion  ->>>>>>>>>>>>>>>
 // + memoisaton  -> >>>>>>>>>>>
@@ -64,28 +32,61 @@ store them
 
 // tabulation -.>>>>>>>>>>>>>
 
-class Solution {
-    vector<vector<int>> dp;
-public:
-    int longestCommonSubsequence(string text1, string text2) {
+// class Solution {
+//     vector<vector<int>> dp;
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
         
-        int n = text1.size(),m = text2.size();
-        dp.assign(n+1,vector<int>(m+1,-1));
+//         int n = text1.size(),m = text2.size();
+//         dp.assign(n+1,vector<int>(m+1,-1));
         
-        // base case // if in declaration of dp, we pass 0, that automatic handle base case;
-        
-        for(int i=0;i<=n;i++)dp[i][0] = 0;
-        for(int j = 0; j<= m;j++)dp[0][j] = 0;
+//         // base case // if in declaration of dp, we pass 0, that automatic handle base case;, no need to write again base cases;
 
-        for(int i=1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+//         for(int i=0;i<=n;i++)dp[i][0] = 0;
+//         for(int j = 0; j<= m;j++)dp[0][j] = 0;
+
+//         for(int i=1;i<=n;i++){
+//             for(int j = 1;j<=m;j++){
+//                 if(text1[i-1] == text2[j-1]){
+//                     dp[i][j] = 1 + dp[i-1][j-1];   /// diag fill the data
+//                 }else{
+//                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]); // other take max of left,top
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// };
+
+
+
+
+class Solution {
+    int n1,n2;
+    vector<vector<int>> dp;
+
+    int solve(int i,int j,string&s1,string&s2){
+
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s1[i] == s2[j]){
+            return dp[i][j] =  1 + solve(i-1,j-1,s1,s2);
+        }else{
+            return dp[i][j] =  max(solve(i-1,j,s1,s2), solve(i,j-1,s1,s2));
         }
-        return dp[n][m];
+    }
+
+public:
+   int longestCommonSubsequence(string s1, string s2) {
+        
+        n1 = s1.size(),n2 = s2.size();
+        dp.assign(n1+1,vector<int>(n2+1,-1) );
+        
+        int len = solve(n1-1,n2-1,s1,s2);
+    
+      return len;
+ 
     }
 };
