abdc9307b6692d75797c9374d854d25e69cdf216
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-01-18 09:05:08 +0530

    Time: 171 ms (25.00%) | Memory: 12.7 MB (96.80%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.cpp
+++ 1311-largest-magic-square/largest-magic-square.cpp
@@ -1,62 +1,77 @@
 #define ll long long
 class Solution {
 
-    bool valid(int i, int n, int j, int m, vector<vector<int>>& mat) {
-        // row sums
+    bool valid (int i,int n,int j,int m,vector<vector<int>>&mat){
+
+        // col sum
         ll sum1 = 0;
-        for (int r = i; r <= n; r++) {
+        for(int r = i;r<=n;r++){
+            // for each row col sum;
             ll temp = 0;
-            for (int c = j; c <= m; c++) {
+            for(int c = j;c<=m;c++){
                 temp += mat[r][c];
             }
-            if (r == i) sum1 = temp;
-            if (sum1 != temp) return false;
+            if(r == i){
+                sum1 = temp;
+            }
+            if(sum1 != temp)return false;
         }
-
-        // column sums
-        for (int c = j; c <= m; c++) {
+        
+        for(int c = j;c<=m;c++){
+            // for each row col sum;
             ll temp = 0;
-            for (int r = i; r <= n; r++) {
+            for(int r = i;r<=n;r++){
                 temp += mat[r][c];
             }
-            if (sum1 != temp) return false;
+            
+           
+            if(sum1 != temp)return false;
+            
         }
 
-        // main diagonal
+        // diagonal
         ll sum2 = 0;
-        int r = i, c = j;
-        while (r <= n && c <= m) {
+        int r = i,c = j;
+        while(r <= n && c <= m){
             sum2 += mat[r][c];
-            r++, c++;
+            r++,c++;
         }
-        if (sum2 != sum1) return false;
+        if(sum2 != sum1)return false;
+        // antidiagonal
+          r = i,c = m;sum2=0;
+        while(r <=n && c >= j){
+            sum2 += mat[r][c];
+            r++,c--;
+        }
 
-        // anti-diagonal
-        sum2 = 0;
-        r = i, c = m;
-        while (r <= n && c >= j) {
-            sum2 += mat[r][c];
-            r++, c--;
-        }
-        if (sum2 != sum1) return false;
+        if(sum2 != sum1)return false;
 
         return true;
+
+
+        
+
     }
 
 public:
     int largestMagicSquare(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
+        int n = mat.size(),m = mat[0].size();
         int ans = 1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int k = 1; i + k < n && j + k < m; k++) {
-                    if (valid(i, i + k, j, j + k, mat)) {
-                        ans = max(ans, k + 1);
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<m;j++){
+
+                for(int k = 1;k<min(n,m);k++){
+
+                    if(i + k < n && j + k < m){
+                        if(valid(i,i+k,j,j+k,mat)){
+                            ans = max(ans,k+1);
+                        }
                     }
                 }
             }
         }
+
         return ans;
     }
 };
