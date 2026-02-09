b6991f4582a1f27c1a59291030f0e9a1c9c0627e
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-02-09 10:18:15 +0530

    Time: 7 ms (65.88%) | Memory: 22.9 MB (79.78%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.cpp
+++ 971-shortest-bridge/shortest-bridge.cpp
@@ -1,64 +1,79 @@
 class Solution {
-    int dx[4] = {0, 0, -1, 1};
-    int dy[4] = {-1, 1, 0, 0};
-    int n, m;
-    vector<vector<bool>> vis;
-    queue<pair<int, int>> q;
+int n,m; 
+queue<pair<int,int>> q;
+vector<vector<bool>> vis;
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
 
-    bool valid(int i, int j) {
-        return (i >= 0 && i < n && j >= 0 && j < m && !vis[i][j]);
-    }
+bool valid(int i,int j){
 
-    void dfs(int r, int c, vector<vector<int>>& grid) {
-        vis[r][c] = true;
-        q.push({r, c});
-        for (int k = 0; k < 4; k++) { // Use 'k' to avoid shadowing
-            int nr = r + dx[k];
-            int nc = c + dy[k];
-            if (valid(nr, nc) && grid[nr][nc] == 1) {
-                dfs(nr, nc, grid);
+    if(i < 0 || j  < 0 || i >= n || j >= m || vis[i][j]== true)return false;
+
+
+    return true;
+}
+
+void dfs(int i,int j,vector<vector<int>>& mat){
+
+    vis[i][j] = true;
+    q.push({i,j});
+
+    for(int k =0;k<4;k++){
+        int n_r = i + dx[k];
+        int n_c = j + dy[k];
+
+        if(valid(n_r,n_c)){
+            if(mat[n_r][n_c]  == 1){
+                dfs(n_r,n_c,mat);
             }
         }
     }
+}
 
 public:
-    int shortestBridge(vector<vector<int>>& grid) {
-        n = grid.size(); m = grid[0].size();
-        vis.assign(n, vector<bool>(m, false));
-
+    int shortestBridge(vector<vector<int>>& mat) {
+        
+        n = mat.size();
+        m = mat[0].size();
+        vis.assign(n,vector<bool>(m,false));
         bool found = false;
-        for (int i = 0; i < n && !found; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    dfs(i, j, grid);
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(mat[i][j] == 1){
+                    dfs(i,j,mat);
                     found = true;
                     break;
                 }
             }
+            if(found)break;
         }
+
+       
+
+
 
         int level = 0;
-        while (!q.empty()) {
+        while(!q.empty()){
             int sz = q.size();
-           
+            while(sz--){
 
-            while (sz--) {
-                auto [r, c] = q.front(); q.pop();
+                auto [r,c] = q.front();q.pop();
 
-                for (int k = 0; k < 4; k++) {
-                    int nr = r + dx[k];
-                    int nc = c + dy[k];
+                 for(int k =0;k<4;k++){
+                        int n_r = r + dx[k];
+                        int n_c = c + dy[k];
 
-                    if (valid(nr, nc)) {
-                        if (grid[nr][nc] == 1) return level; // Second island hit!
-                        
-                        vis[nr][nc] = true;
-                        q.push({nr, nc});
-                    }
+                        if(valid(n_r,n_c)){
+
+                            if(mat[n_r][n_c] == 1)return level;
+                            q.push({n_r,n_c});
+                            vis[n_r][n_c] = true;
+                        }
                 }
             }
-             level++;
+            level++;
         }
+
         return level;
     }
 };
