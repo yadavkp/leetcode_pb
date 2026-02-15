8a4ec81f4da79eec0016940bde4d76e8ea2babfc
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-02-15 11:15:47 +0530

    Time: 303 ms (9.26%) | Memory: 206.6 MB (5.12%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,19 +1,30 @@
 class Solution {
 
-    bool dfs(int st,int end,vector<int>&vis, vector<vector<int>>&adj){
+   
+    bool bfs(int st,int end, vector<int>&vis , vector<vector<int>>&adj){
+
+        queue<int> q;q.push(st);
+
         vis[st] = 1;
+        
 
-        if(st == end)return true;
+       while(!q.empty()){
 
-        for(auto nr : adj[st]){
-            if(!vis[nr]){
-                if(dfs(nr,end,vis,adj)) return true;
+            auto cur = q.front(); q.pop();
+
+            if(cur == end)return true;
+
+             for(auto nr : adj[cur]){
+                
+                 if(!vis[nr]){
+                    q.push(nr);
+                    vis[nr] = 1;
+                 }
             }
-        }
+       }
 
         return false;
     }
-
 
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& query) {
@@ -28,8 +39,8 @@ public:
 
         for(int j = 0;j<query.size();j++){
             auto vec = query[j];
-            vector<int> vis(n,0);
-            bool found = dfs(vec[0],vec[1],vis,adj);
+            vector<int>vis(n,0);
+            bool found = bfs(vec[0],vec[1],vis,adj);
             ans[j] = found;
         }
 
@@ -37,3 +48,45 @@ public:
     
     }
 };
+
+
+
+// class Solution {
+
+//     bool dfs(int st,int end,vector<int>&vis, vector<vector<int>>&adj){
+//         vis[st] = 1;
+
+//         if(st == end)return true;
+
+//         for(auto nr : adj[st]){
+//             if(!vis[nr]){
+//                 if(dfs(nr,end,vis,adj)) return true;
+//             }
+//         }
+
+//         return false;
+//     }
+
+
+// public:
+//     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& query) {
+        
+//         vector<vector<int>> adj(n);
+
+//         for(auto vec : pre){
+//             adj[vec[0]].push_back(vec[1]);
+//         }
+
+//         vector<bool> ans(query.size(),false);
+
+//         for(int j = 0;j<query.size();j++){
+//             auto vec = query[j];
+//             vector<int> vis(n,0);
+//             bool found = dfs(vec[0],vec[1],vis,adj);
+//             ans[j] = found;
+//         }
+
+//         return ans;
+    
+//     }
+// };
