6321f367889a4438b9ab35c040966a472e3f8be3
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-02-15 12:20:57 +0530

    Time: 85 ms (34.42%) | Memory: 80.5 MB (16.73%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,53 +1,110 @@
+
+// m-1 ->  by dfs
+// m - 2 _>  by bfs
+// m-3 ->    by topoorder
+// m-4 ->  by the preprocess preq in the matrix , check is it true or not
+
+
+
 class Solution {
-
-   
-    bool bfs(int st,int end, vector<int>&vis , vector<vector<int>>&adj){
-
-        queue<int> q;q.push(st);
-
-        vis[st] = 1;
-        
-
-       while(!q.empty()){
-
-            auto cur = q.front(); q.pop();
-
-            if(cur == end)return true;
-
-             for(auto nr : adj[cur]){
-                
-                 if(!vis[nr]){
-                    q.push(nr);
-                    vis[nr] = 1;
-                 }
-            }
-       }
-
-        return false;
-    }
 
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& query) {
         
         vector<vector<int>> adj(n);
 
+        vector<int> ind(n,0);
         for(auto vec : pre){
             adj[vec[0]].push_back(vec[1]);
+            ind[vec[1]]++;
+        }
+            queue<int> q;
+        for(int i=0;i<n;i++){
+            if(ind[i] == 0)q.push(i);
         }
 
         vector<bool> ans(query.size(),false);
 
+        unordered_map<int,set<int>> mp;
+
+        while(!q.empty()){
+
+            auto cur = q.front(); q.pop();
+
+            for(auto nr : adj[cur]){
+                ind[nr]--;
+                if(ind[nr]==0)q.push(nr);
+
+                mp[nr].insert(cur);
+                // proces prerequiset
+                for(auto preq : mp[cur]){
+                    mp[nr].insert(preq);
+                }
+            }
+        }
+
         for(int j = 0;j<query.size();j++){
             auto vec = query[j];
-            vector<int>vis(n,0);
-            bool found = bfs(vec[0],vec[1],vis,adj);
-            ans[j] = found;
+            int u = vec[0],v = vec[1];
+            bool fnd = mp[v].count(u);
+            ans[j] = fnd;
         }
 
         return ans;
     
     }
 };
+
+// class Solution {
+
+   
+//     bool bfs(int st,int end, vector<int>&vis , vector<vector<int>>&adj){
+
+//         queue<int> q;q.push(st);
+
+//         vis[st] = 1;
+        
+
+//        while(!q.empty()){
+
+//             auto cur = q.front(); q.pop();
+
+//             if(cur == end)return true;
+
+//              for(auto nr : adj[cur]){
+                
+//                  if(!vis[nr]){
+//                     q.push(nr);
+//                     vis[nr] = 1;
+//                  }
+//             }
+//        }
+
+//         return false;
+//     }
+
+// public:
+//     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& query) {
+        
+//         vector<vector<int>> adj(n);
+
+//         for(auto vec : pre){
+//             adj[vec[0]].push_back(vec[1]);
+//         }
+
+//         vector<bool> ans(query.size(),false);
+
+//         for(int j = 0;j<query.size();j++){
+//             auto vec = query[j];
+//             vector<int>vis(n,0);
+//             bool found = bfs(vec[0],vec[1],vis,adj);
+//             ans[j] = found;
+//         }
+
+//         return ans;
+    
+//     }
+// };
 
 
 
