ce8f1b365978c80e5a06b5d897344630369c38db
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-02-19 12:42:59 +0530

    Time: 68 ms (40.44%) | Memory: 21.3 MB (60.32%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,74 +1,3 @@
-// class Solution {
-//     int dx[4] = {0,0,-1,1};
-//     int dy[4] = {-1,1,0,0};
-// public:
-//     int shortestPath(vector<vector<int>>& grid, int k) {
-//         int n = grid.size(), m = grid[0].size();
-
-//         vector<vector<int>> vis(n,vector<int>(m,0));
-
-//         if(grid[0][0] == 1){
-//             k--;
-//         }
-
-//         queue<pair<int,int>> q;q.push({0,0});
-
-
-//         vis[0][0] = 1;
-
-//         auto valid = [&](int r,int c){
-
-//             if(r  < 0 || r >= n || c <0 || c >= m || vis[r][c] == 1) return false;
-
-//             return true;
-
-//         };
-
-//         int len = 0;
-//         while(!q.empty()){
-
-//             int sz = q.size();
-//             int k1 = k;
-
-//             while(sz--){
-
-//                 auto [r,c] = q.front(); q.pop();
-//                 if(r == n-1 && c == m-1){
-//                     return len;
-//                 }
-//                 bool fnd = false;
-//                 for(int i=0;i<4;i++){
-//                     int n_r = r + dx[i];
-//                     int n_c = c + dy[i];
-//                     if(valid(n_r,n_c)){
-
-//                         if( grid[n_r][n_c] == 1 && k1 > 0){
-//                             fnd = true;
-//                             q.push({n_r,n_c});
-//                             vis[n_r][n_c] = 1;
-//                         }else if(grid[n_r][n_c] == 0){
-
-//                             q.push({n_r,n_c});
-//                             vis[n_r][n_c] = 1;
-//                         }
-
-//                         // cout<<"r-> "<<n_r<<" c-> "<<n_c<<" \n";
-//                     }
-//                 }
-//                 if(fnd && k1 >0){
-//                         k1--;
-//                 }
-
-//             }
-//             k = k1;
-//             len++;
-//         }
-
-//         return -1;
-//     }
-// };
-
-
 
 class Solution {
     int dx[4] = {0,0,-1,1};
@@ -117,7 +46,7 @@ public:
 
                         int next_k = rem_k - grid[n_r][n_c];
 
-                        if(next_k >= 0 && next_k > vis[n_r][n_c]){
+                        if(next_k >= 0 && next_k > vis[n_r][n_c]){  // 0 > -1
                             vis[n_r][n_c] = next_k;
                             pq.push({steps+1,n_r , n_c , next_k});
                         }
