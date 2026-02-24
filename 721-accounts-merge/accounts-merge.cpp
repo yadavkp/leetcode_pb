d419c84a5adc7fb7ece45c18f4b38c3e6e8c91d8
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-02-24 19:57:50 +0530

    Time: 38 ms (47.32%) | Memory: 39.4 MB (84.16%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.cpp
@@ -0,0 +1,71 @@
+class Solution {
+    #define vi vector<int>
+    #define pb push_back
+
+
+    int findpar(int nod,vi&par){
+        if(par[nod] == nod)return nod;
+
+        return par[nod] = findpar(par[nod],par);
+    }
+
+    void unite (int u,int v, vi&par,vi&size){
+        int pu = findpar(u,par);
+        int pv = findpar(v,par);
+
+
+        if(pu == pv) return;
+
+        if(size[pu] < size[pv]){
+            par[pu] = pv;
+            size[pv]+=size[pu];
+        }else{
+            par[pv] = pu;
+            size[pu]+=size[pv];
+        }
+    }
+
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+        int n = accounts.size();
+        vi par(n);
+        vi size(n,1);
+        for(int i=0;i<n;i++) par[i] = i;
+
+        unordered_map<string,int> em_idx;
+
+        for(int i=0;i<n;i++){
+            for(int j = 1;j<accounts[i].size();j++){
+                string email = accounts[i][j];
+
+                if(em_idx.count(email)){
+                    unite(i,em_idx[email],par,size);
+                }else{
+                    em_idx[email] = i;
+                }
+            }
+        }
+
+        unordered_map<int,vector<string>> merg_grp;
+
+        for(auto const&[email,idx] : em_idx){
+            int leader = findpar(idx,par);
+            merg_grp[leader].pb(email);
+        }
+
+        vector<vector<string>> res;
+
+        for(auto [idx,email] : merg_grp){
+            sort(email.begin(),email.end());
+
+            vector<string> row;
+            row.pb(accounts[idx][0]);//
+            row.insert(row.end(),email.begin(),email.end());
+
+            res.pb(row);
+        }
+
+        return res;
+    }
+};
