305c4736ed7281154383b106aa73f5048269bd6a
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-08 09:57:50 +0530

    Time: 1431 ms (6.73%) | Memory: 426.8 MB (5.08%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+
+    void pre_gen_g_cod(int n,vector<string>&ans){
+
+       for(int mask = 0; mask < (1 << n);mask++){
+         string s ;
+         for(int j = n-1;j>=0;j--){
+            s += (((mask >> j) == 1) ? "1" : "0");
+         }
+         ans.push_back(s);
+       }
+
+    }
+
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        // step generate gray code, then , do work 
+        int n = nums.size();
+        vector<string> ans;
+        pre_gen_g_cod(n,ans);
+        set<string> temp(ans.begin(),ans.end());
+        
+       
+        
+        
+        for(auto it : nums){
+             if(temp.count(it)){
+                //cout<<it<<" ";
+                temp.erase(it);
+                
+             }
+        }
+       for(auto t : temp){
+        return t;
+       }
+        return "";
+
+    }
+};
