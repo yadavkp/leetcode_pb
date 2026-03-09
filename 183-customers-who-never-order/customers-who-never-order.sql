d3f35c711b0f879d01cf733b8667dab35dca76b6
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-09 16:23:39 +0530

    Time: 537 ms (57.22%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -5,8 +5,14 @@
 -- foreign key   id references Orders(customerId)
 -- where id != customerId ;
 
+-- select a.name as Customers
+-- from Customers as a
+-- where id NOT IN (
+--     select customerId from Orders
+-- ) ;
+
 select a.name as Customers
-from Customers as a
-where id NOT IN (
-    select customerId from Orders
-) ;
+from Customers as a 
+LEFT JOIN Orders b
+on a.id  = b.customerId
+where b.customerId is null;
