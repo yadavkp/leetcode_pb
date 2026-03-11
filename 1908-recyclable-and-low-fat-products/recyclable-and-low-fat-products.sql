b7786c083e0da0d9c30d06d847491bd68aad7675
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-12 04:17:23 +0530

    Time: 480 ms (87.14%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select a.product_id as product_id
 from Products as a
--- left join Products as b
--- on  a.low_fats = b.recyclable
+left join Products as b
+on  a.product_id = b.product_id
 where a.low_fats = "Y" and a.recyclable = "Y";
