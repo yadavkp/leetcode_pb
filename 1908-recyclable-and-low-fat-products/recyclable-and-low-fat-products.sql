6f25772fb3f872c958c6f044795740e2330181a7
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-12 04:52:40 +0530

    Time: 497 ms (72.16%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -4,10 +4,32 @@
 -- from Products as a
 -- where a.low_fats = "Y" and a.recyclable ="Y";
 
-select a.product_id as product_id
-from Products as a
-where a.product_id in(
-    select product_id 
-    from Products
-    where low_fats ="y" and recyclable = "Y"
-);
+-- 2nd method subquery 
+
+-- select a.product_id as product_id
+-- from Products as a
+-- where a.product_id in(
+--     select product_id 
+--     from Products
+--     where low_fats ="y" and recyclable = "Y"
+-- );
+
+
+-- 3rd CTE WAY 
+
+-- with temp AS (
+--     -- your query what u want to write here
+--     select product_id from Products
+--     where low_fats = "Y"  and recyclable = "Y"
+-- )
+-- select product_id from temp;
+
+-- 4th way to write by the Intersect way of two lists
+
+select product_id 
+from Products
+ where low_fats = 'Y'
+intersect
+select product_id 
+from Products
+ where recyclable = 'Y';
