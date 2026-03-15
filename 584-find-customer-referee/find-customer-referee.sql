8100da38dfdc09821c6c9c23ea042be3fc80bd15
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-15 15:11:17 +0530

    Time: 451 ms (71.26%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,13 @@
 # Write your MySQL query statement below
-select a.name 
-from Customer as a
-where a.referee_id != 2 or a.referee_id is null;
+
+-- select a.name 
+-- from Customer as a
+-- where a.referee_id != 2 or a.referee_id is null;
+
+-- select name 
+-- from Customer 
+-- where ifnull(referee_id,0) != 2;
+
+select name 
+from Customer 
+where coalesce(referee_id,0) != 2;
