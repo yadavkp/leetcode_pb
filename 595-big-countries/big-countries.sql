4ccfacadad852da3decfd5eb7d4544f176d75134
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-15 16:11:38 +0530

    Time: 375 ms (9.97%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,5 +1,21 @@
 # Write your MySQL query statement below
 
-select a.name, a.population, a.area
-from World as a
-where a.area >= 3000000 or a.population >=25000000
+-- using or 
+
+-- select a.name, a.population, a.area
+-- from World as a
+-- where a.area >= 3000000 or a.population >=25000000
+
+
+-- using union , this is fast by the or
+
+select name ,population , area
+from World
+where area >= 3000000
+
+union 
+
+select name ,population , area
+from World
+where population >= 25000000;
+
