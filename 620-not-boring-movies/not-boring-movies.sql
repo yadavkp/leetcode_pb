a2f70670ac8bf62701f836032449b45848d56b6c
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-18 15:57:19 +0530

    Time: 316 ms (10.72%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -5,10 +5,17 @@
 -- where id%2!= 0 and description != "boring"
 -- order by rating desc;
 
-with temp as (
-    select id , movie,description,rating
+-- with temp as (
+--     select id , movie,description,rating
+--     from cinema
+--     where id%2 != 0 and description != "boring"
+-- )
+-- select * from temp
+-- order by rating desc;
+
+
+select * from 
+    (select id , movie,description,rating
     from cinema
-    where id%2 != 0 and description != "boring"
-)
-select * from temp
+    where id%2 != 0 and description != "boring") t
 order by rating desc;
