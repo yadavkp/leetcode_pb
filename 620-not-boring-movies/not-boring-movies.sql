e65d8c84755ef22911265713896da72585e052ee
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-18 15:54:20 +0530

    Time: 275 ms (24.52%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
 
-select id, movie,description ,rating
-from Cinema
-where id%2!= 0 and description != "boring"
+-- select id, movie,description ,rating
+-- from Cinema
+-- where id%2!= 0 and description != "boring"
+-- order by rating desc;
+
+with temp as (
+    select id , movie,description,rating
+    from cinema
+    where id%2 != 0 and description != "boring"
+)
+select * from temp
 order by rating desc;
