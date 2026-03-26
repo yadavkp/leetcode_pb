859916c050aecfbb26f0935e78db22c738eb6df0
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-26 22:03:05 +0530

    Time: 1109 ms (17.75%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -12,6 +12,7 @@ with temp  as (
     from Employee as a
     left join Bonus as b
     on a.empId = b.empId
-    where b.bonus is null or b.bonus < 1000
+    where ifnull(b.bonus,0) < 1000
+    -- //where b.bonus is null or b.bonus < 1000
 )
 select * from temp;
