bd853493fa109399941ff5503161ab6f8f84281d
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-27 23:27:28 +0530

    Time: 147 ms (79.62%) | Memory: 98.9 MB (77.79%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,32 +1,59 @@
-//policy based data structures (pbds)
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template<typename T>
-using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//for duplicates allow use less_equal<T>
-
-// s.find_by_order(k) → iterator to k-th element (0-indexed)
-// s.order_of_key(x) → count of elements strictly smaller than x
-
-
 class Solution {
+
+    vector<int> seg;
+    int find_query(int idx,int ql,int qr,int l,int r){
+
+        if(r  <ql || qr < l) return 0;
+
+        if(ql <= l && r <= qr)return seg[idx];
+
+       int mid =  l + (r - l)/2;
+
+        return find_query(2*idx+1,ql,qr,l,mid) + find_query(2*idx+2,ql,qr,mid+1,r);
+    }
+
+    void update(int idx,int i,int l,int r){
+
+        if(l == r){
+            
+            seg[idx] += 1;
+           
+            return ;
+        }
+
+        int mid = l+ (r - l)/2;
+
+        if(i <= mid)update(2*idx+1,i,l,mid);
+        else update(2*idx+2,i,mid+1,r);
+
+        seg[idx] = seg[2*idx+1] + seg[2*idx+2];
+
+    }
+
 public:
     vector<int> countSmaller(vector<int>& nums) {
+
+          
+        reverse(nums.begin(),nums.end());
         int n = nums.size();
-        ordered_set<int> seen;
-        vector<int> ans(n);
+        seg.assign(4*n,0);
+        vector<int> temp = nums;
+        sort(temp.begin(),temp.end());
 
-        for(int i=n-1;i>=0;i--){
-            if(seen.empty()){
-                ans[i] = 0;
-            }else{
-                auto it = seen.order_of_key(nums[i]);
-                ans[i] = it;
-            }
+        temp.erase(unique(temp.begin(),temp.end()),temp.end());
+        vector<int> ans(n,0);
 
-            seen.insert(nums[i]);
+        for(int i=0;i<n;i++){
+
+            auto idx = upper_bound(temp.begin(),temp.end(),nums[i]) - temp.begin()-1;
+           // cout<<idx<<" ";
+
+            int cnt = find_query(0,0,idx-1,0,temp.size()-1);
+            ans[i] = cnt;
+            update(0,idx,0,temp.size()-1);
+
         }
+        reverse(ans.begin(),ans.end());
 
         return ans;
     }
