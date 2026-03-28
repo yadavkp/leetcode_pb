7bed7a40b89ce5152da06a233a45f2147add8be2
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-03-28 23:25:15 +0530

    Time: 901 ms (72.39%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,20 +1,20 @@
 # Write your MySQL query statement below
 
--- select a.user_id,a.user_name,b.contest_id
--- from Users as  a
--- right join Register as b
--- on a.user_id = b.user_id;
 
--- select a.contest_id,b.user_id,b.user_name
+
+-- select  distinct a.contest_id
+--         , round(count(b.user_id) *100.0 / (select count(*) from Users) , 2) as percentage
 -- from Register as a
 -- left join Users as b
--- on a.user_id = b.user_id;
+-- on a.user_id = b.user_id
+-- group by a.contest_id
+-- order by  percentage desc, a.contest_id asc; 
 
+select contest_id,
+        Round(
+            count(user_id) * 100.0 /  (select count(*) from Users),2
 
-select  distinct a.contest_id
-        , round(count(b.user_id) *100.0 / (select count(*) from Users) , 2) as percentage
-from Register as a
-left join Users as b
-on a.user_id = b.user_id
-group by a.contest_id
-order by  percentage desc, a.contest_id asc; 
+        ) as percentage 
+from Register 
+group by contest_id
+order by percentage desc, contest_id asc;
