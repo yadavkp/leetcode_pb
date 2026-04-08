bac74da36e7e1541992370cdb1edba4feb838e07
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-08 07:24:01 +0530

    Time: 137 ms (15.57%) | Memory: 42.4 MB (19.26%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,4 +1,6 @@
 class Solution {
+
+
     int dp[51][51][51][51];
     int n;
     bool is_out_of_bond(int r1,int c1,int r2,int c2,vector<vector<int>>& grid ){
@@ -12,11 +14,11 @@ class Solution {
         if(is_out_of_bond(r1,c1,r2,c2,grid)) return -1e9;
 
 
-        if(r1 == n-1 && c1 == n-1)return grid[r1][c1];
+        if(r1 == n-1 && c1 == n-1)return grid[r1][c1]; // you reached to the end  cell// if single person reachd it means other also reached
 
         if(dp[r1][c1][r2][c2] != -1) return dp[r1][c1][r2][c2];
 
-        int cherry = -1e9;
+        int cherry = 0;
         if(r1 == r2 && c1 == c2)cherry = grid[r1][c1];
         else cherry = grid[r1][c1] + grid[r2][c2];
 
@@ -27,16 +29,18 @@ class Solution {
         int f3 = solve(r1+1,c1,r2,c2+1,grid); //d//r
         int f4 = solve(r1,c1+1,r2+1,c2,grid); //r//d
 
-        if( max({f1,f2,f3,f4}) == -1e9) return dp[r1][c1][r2][c2] = -1e9;
+        if(max({f1,f2,f3,f4}) == -1e9) return dp[r1][c1][r2][c2] = -1e9;
         return dp[r1][c1][r2][c2] = cherry + max({f1,f2,f3,f4});
 
 
     }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
+
         n = grid.size();
         memset(dp,-1,sizeof(dp));
         int ans= solve(0,0,0,0,grid) ;
         return (ans == -1e9 ? 0 : ans);
+
     }
 };
