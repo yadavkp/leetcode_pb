01ef54c4967629c87fa75d3b48fa1c0caf0edff8
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-12 18:35:48 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (93.70%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
@@ -9,7 +9,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j] == 1){
-                    ans[j]++;
+                    ans[i]++;
                 }
             }
         }
