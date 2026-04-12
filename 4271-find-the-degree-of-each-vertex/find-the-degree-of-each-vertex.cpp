df3ce08cb7e0641fddf24428ce5e60a8b5dba22c
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-12 18:35:30 +0530

    Time: 8 ms (14.11%) | Memory: 54.4 MB (73.47%) - LeetSync

---
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        
+        // used the concept of topological order ->
+
+        int n = matrix.size();
+        vector<int> ans(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 1){
+                    ans[j]++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
