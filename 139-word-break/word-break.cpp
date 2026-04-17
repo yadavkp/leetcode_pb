51befd309b97f1dbb8163bb180e1ecc6926c076d
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-17 18:49:43 +0530

    Time: 17 ms (17.25%) | Memory: 16.9 MB (27.70%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -7,12 +7,13 @@ class Solution {
 
         if(memo[i] != -1) return memo[i];
         bool ans = false;
-        for(int j=i+1;j<=s.size();j++){
-           string pfx = s.substr(i,j - i);
-           if(dict.count(pfx) && solve(j,s,dict)){
+        for(int j=i;j<s.size();j++){
+           string pfx = s.substr(i,(j - i)+1);
+           if(dict.count(pfx) && solve(j+1,s,dict)){
              return  memo[i] = true;
            }
         }
+
         return memo[i] = false;;
     }
 public:
