afc23b184266ef9e9a411ad5782e3e5dfa544141
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-17 18:44:07 +0530

    Time: 15 ms (27.88%) | Memory: 16.9 MB (26.32%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int n;
+    int memo[200];
+    bool solve(int i,string&s,unordered_set<string>&dict ){
+
+        if(i >= n)return true;
+
+        //if(dict.count(s)) return true;
+        if(memo[i] != -1) return memo[i];
+        bool ans = false;
+        for(int j=i+1;j<=s.size();j++){
+           string pfx = s.substr(i,j - i);
+           if(dict.count(pfx) && solve(j,s,dict)){
+             return  memo[i] = true;
+           }
+        }
+        return memo[i] = false;;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+            n = s.size();
+            unordered_set<string> seen ;
+            memset(memo,-1,sizeof(memo));
+            seen.insert(wordDict.begin(),wordDict.end());
+
+        return solve(0,s,seen);
+    }
+};
