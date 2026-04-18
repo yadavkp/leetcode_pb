7611e9137fa9235fd78ed7192bfb8c02254ae566
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-18 09:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (19.66%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -5,12 +5,26 @@ public:
         vector<int> dp(n,1e7);
         dp[0] = 0;
 
-        for(int i=0;i<n;i++){
-             for(int j = i + 1;j <= min(n-1,i + nums[i]);j++){
-                dp[j] = min(dp[j],dp[i]+1);
+        // for(int i=0;i<n;i++){
+        //      for(int j = i + 1;j <= min(n-1,i + nums[i]);j++){
+        //         dp[j] = min(dp[j],dp[i]+1);
+        //      }
+        // }
+
+        // return dp[n-1];
+        int curr_end= 0,mx_reach = 0,jump = 0;
+      
+
+
+        for(int i=0;i<n-1;i++){
+             
+             mx_reach = max(mx_reach,i + nums[i]);
+
+             if(i == curr_end){
+                jump++;
+                curr_end = mx_reach;
              }
         }
-
-        return dp[n-1];
+        return jump;
     }
 };
