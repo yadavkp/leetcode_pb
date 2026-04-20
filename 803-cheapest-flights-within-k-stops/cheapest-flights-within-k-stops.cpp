0af2252d80674d98624e14e58d95484823badacb
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-04-20 18:31:36 +0530

    Time: 16 ms (10.25%) | Memory: 19.6 MB (13.85%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -27,7 +27,7 @@ public:
 
             if(u == dst)return d;
 
-            if(d > dist[u][kk]) continue;
+           // if(d > dist[u][kk]) continue;
 
             for(auto [w,v] : adj[u]){
 
@@ -43,38 +43,3 @@ public:
     }
 };
 
-
-// class Solution {
-//     #define ll long long
-//     #define pb push_back
-// public:
-//     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-
-//         vector<vector<pair<ll,ll>>>  adj(n);
-
-//         for(auto vec : flights){
-//             int u = vec[0],v = vec[1],w = vec[2];
-
-//             adj[u].pb({w,v});
-//         }
-        
-//         using T = tupe<ll,ll,ll>;
-//         priority_queue<T, vector<T> , greater<T> > pq;
-
-//         vector<vector<ll>> dist(n,1e18);
-
-//         dist[src][k] = 0;
-//         pq.push({0,src,k});
-
-//         while(!pq.empty()){
-//             auto [d,u,kk] = pq.top(); pq.pop();
-
-//             if(u == dst)return d;
-
-//             for(auto [w,v] : adj[u]){
-
-//                 if(dist[v][kk] > 0 && dist[v][])
-//             }
-//         }
-//     }
-// };
