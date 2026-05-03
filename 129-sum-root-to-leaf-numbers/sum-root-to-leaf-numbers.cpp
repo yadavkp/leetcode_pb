65ff56259c79db97ef2b0338425adc35966d32e4
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-03 17:01:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (68.36%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -9,28 +9,60 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// class Solution {
+//     int ans = 0;
+
+//     void solve(TreeNode* root,int sum){
+
+//         if(!root) return ;
+        
+//         if(!root->left && !root->right){
+//             ans += sum;
+//             return;
+//         }
+
+//         if(root->left)solve(root->left,sum*10 + root->left->val);
+//         if(root->right)solve(root->right,sum*10 + root->right->val);
+
+//     }
+
+// public:
+//     int sumNumbers(TreeNode* root) {
+//         int sum = 0;
+//         solve(root,root->val);
+
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
-    int ans = 0;
+    
 
-    void solve(TreeNode* root,int sum){
+    int  solve(TreeNode* root,int sum){
 
-        if(!root) return ;
-
+        if(!root) return 0 ;
+        int ans = 0;
         if(!root->left && !root->right){
-            ans += sum;
-            return;
+            return sum;
+            
         }
 
-        if(root->left)solve(root->left,sum*10 + root->left->val);
-        if(root->right)solve(root->right,sum*10 + root->right->val);
+        if(root->left)ans += solve(root->left,sum*10 + root->left->val);
+        if(root->right)ans += solve(root->right,sum*10 + root->right->val);
+
+        return ans;
 
     }
 
 public:
     int sumNumbers(TreeNode* root) {
         int sum = 0;
-        solve(root,root->val);
+        return solve(root,root->val);
 
-        return ans;
+        
     }
 };
