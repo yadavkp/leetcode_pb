a31ae3db48b802346764ca05a15615ddcc4cb836
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-07 22:28:53 +0530

    Time: 352 ms (23.44%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,6 +1,16 @@
 # Write your MySQL query statement below
 
-select class 
-from Courses 
-group by class
-having count(*) >4;
+
+-- select class 
+-- from Courses 
+-- group by class
+-- having count(*) > 4 ;
+
+
+select class
+from (
+    select class , count(class) as cnt
+    from courses
+    group by class
+) as temp
+where cnt >= 5;
