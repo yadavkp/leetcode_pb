7eebf90b23091506a6f896275b64938452a0daaf
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-08 12:28:43 +0530

    Time: 594 ms (23.44%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,6 +1,17 @@
 # Write your MySQL query statement below
 
-select user_id , count( distinct follower_id) as followers_count
-from Followers 
-group by user_id
+-- select user_id , count( distinct follower_id) as followers_count
+-- from Followers 
+-- group by user_id
+-- order by user_id asc;
+
+--  solve ths pb by the subquery 
+
+select * 
+from (
+    select user_id, count(distinct follower_id) as followers_count
+    from Followers
+    group by user_id
+    order by user_id asc
+) as temp 
 order by user_id asc;
