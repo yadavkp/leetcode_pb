fe0bfe66df82ef914aa0a87e4c33df7a8bf699b1
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-09 22:41:49 +0530

    Time: 380 ms (21.96%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,22 +1,27 @@
-# Write your MySQL query statement below
--- select a.name , count(a.managerId)
--- from Employee as a
--- left join Employee as b
--- on a.id = b.id
--- where a.id = b.managerId
--- group by a.managerId;
-
-
--- select a.name , count(a.managerId)
--- from Employee as a
---   (select )
+-- this by the cte , 
 
   with temp as (
-    select managerId, count(managerId) as count from  Employee where managerId is not null
+    select managerId, count(managerId) as cnt from  Employee where managerId is not null
     group by managerId
   )
   select name from Employee as a
   left join temp as b
   on a.id  = b.managerId
-  where b.count >= 5;
+  where b.cnt >= 5;
+
+
   
+-- now will solve by inline query
+
+-- solve by the window function ->
+
+
+
+-- SELECT DISTINCT name
+-- FROM (
+--     SELECT a.name,
+--            COUNT(e.id) OVER(PARTITION BY a.id) as cnt
+--     FROM Employee a
+--     INNER JOIN Employee e ON a.id = e.managerId
+-- ) t
+-- WHERE cnt >= 5;
