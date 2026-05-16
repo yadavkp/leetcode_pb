fa7e2d5d589826dd65b60096f0670df64fbbce78
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-16 22:35:56 +0530

    Time: 754 ms (15.66%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -10,12 +10,35 @@
 -- inner join Employees as b
 -- on 
 
-select m.employee_id,
-        m.name,
-        count(e.reports_to) as reports_count,
-        round(avg(e.age)) as average_age
-from Employees as e
-join Employees as m
-on e.reports_to = m.employee_id
-        group by e.reports_to
-        order by m.employee_id
+-- select m.employee_id,
+--         m.name,
+--         count(e.reports_to) as reports_count,
+--         round(avg(e.age)) as average_age
+-- from Employees as e
+-- join Employees as m
+-- on e.reports_to = m.employee_id
+--         group by e.reports_to
+--         order by m.employee_id
+
+-- 2 nd way to solve this bp 
+
+-- select distinct
+--     m.employee_id,
+--     m.name,
+--     count(e.employee_id) over(partition by e.reports_to) as reports_count,
+--     round(avg(e.age)) over (partition by e.reports_to) as average_age
+-- from Employees as e
+-- join Employees as m
+--   on e.reports_to = m.employee_id
+--   order by m.employee_id;
+
+  SELECT DISTINCT
+    m.employee_id,
+    m.name,
+    COUNT(e.employee_id) OVER (PARTITION BY e.reports_to) AS reports_count,
+    ROUND(AVG(e.age) OVER (PARTITION BY e.reports_to)) AS average_age
+FROM Employees AS e
+JOIN Employees AS m
+  ON e.reports_to = m.employee_id
+ORDER BY m.employee_id;
+
