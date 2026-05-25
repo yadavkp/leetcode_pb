814c9f60666070101399d1f74181d0239ca9223e
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-25 16:59:49 +0530

    Time: 3441 ms (5.25%) | Memory: 92.8 MB (5.42%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        
+        int p = minJump,q = maxJump,n = s.size();
+        vector<int> reach(n,0);
+        set<int> seen;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                seen.insert(i);
+            }
+        }
+        reach[0] =1;
+         for(int i=0;i<n;i++){
+            if(reach[i]){
+                int mn = i + p,mx = i+q;
+                auto lb =seen.lower_bound(mn);
+                auto up = seen.upper_bound(mx);
+                
+                if(lb == seen.end()) continue;
+                
+                while(!seen.empty() && lb != up ){
+                    auto next = std ::next(lb);
+                    reach[*lb] = 1;
+
+                    seen.erase(lb);
+                    lb = next;
+                }
+            }
+            cout<<reach[i]<<" ";
+        }
+
+        return reach[n-1]==1;
+
+    }
+};
