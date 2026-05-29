a3feb9777727688378e2075c379cf26e746ae54a
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-29 12:14:45 +0530

    Time: 639 ms (16.65%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -6,8 +6,15 @@
 -- );
 
 
+-- select distinct l1.num as ConsecutiveNums
+-- from Logs l1, Logs l2, Logs l3
+-- where l1.id = l2.id-1 and l2.id = l3.id-1
+--  and l1.num = l2.num and l2.num = l3.num;
+
+
 select distinct l1.num as ConsecutiveNums
-from Logs l1, Logs l2, Logs l3
-where l1.id = l2.id-1 and l2.id = l3.id-1
- and l1.num = l2.num and l2.num = l3.num;
+ from Logs l1
+ join Logs  l2 on l1.id = l2.id-1
+ join Logs l3 on l2.id = l3.id-1
+ where l1.num = l2.num and l2.num = l3.num;
 
