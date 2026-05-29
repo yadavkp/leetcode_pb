17164bea2cdbe1cc22a92275d79988ae79022a7e
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-29 12:16:56 +0530

    Time: 584 ms (38.98%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -11,10 +11,22 @@
 -- where l1.id = l2.id-1 and l2.id = l3.id-1
 --  and l1.num = l2.num and l2.num = l3.num;
 
+-- v can also compare and by subtacting 1 in the second colm
 
-select distinct l1.num as ConsecutiveNums
+
+-- select distinct l1.num as ConsecutiveNums
+--  from Logs l1
+--  join Logs  l2 on l1.id = l2.id-1
+--  join Logs l3 on l2.id = l3.id-1
+--  where l1.num = l2.num and l2.num = l3.num;
+
+
+
+-- v can also add in first colm and then compare 
+
+ select distinct l1.num as ConsecutiveNums
  from Logs l1
- join Logs  l2 on l1.id = l2.id-1
- join Logs l3 on l2.id = l3.id-1
+ join Logs  l2 on l1.id+1 = l2.id
+ join Logs l3 on l2.id + 1 = l3.id
  where l1.num = l2.num and l2.num = l3.num;
 
