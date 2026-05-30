4035f536f147d3369a33d22f62600b2cde8fe496
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-05-30 22:54:10 +0530

    Time: 319 ms (50.73%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -2,16 +2,16 @@
 -- when colm name in the table same as any sql keyword- > then quotes that it simple
 --  ex = "rank"
 
--- select round(score,2) as score,
---     dense_rank() over (order by score desc) as ran
--- from Scores;
+select round(score,2) as score,
+    dense_rank() over (order by score desc) as "rank"
+from Scores;
 
 
-WITH Rounded AS (
-    SELECT ROUND(score, 2) AS rounded_score
-    FROM Scores
-)
-SELECT 
-    rounded_score as score,
-    DENSE_RANK() OVER (ORDER BY rounded_score DESC) AS "rank"
-FROM Rounded;
+-- WITH Rounded AS (
+--     SELECT ROUND(score, 2) AS rounded_score
+--     FROM Scores
+-- )
+-- SELECT 
+--     rounded_score as score,
+--     DENSE_RANK() OVER (ORDER BY rounded_score DESC) AS "rank"
+-- FROM Rounded;
