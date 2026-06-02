848a491e6c4bc531dc6c4213c277c02c54b279b8
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-06-02 22:16:58 +0530

    Time: 71 ms (12.98%) | Memory: 130.6 MB (7.67%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -11,15 +11,46 @@
  */
 
 
+//  class Solution {
+
+//     #define ll long long
+//     int mod = 1e9 + 7;
+//     ll tot_sum = 0, ans = 0;
+
+//     ll getsum(TreeNode* root) {
+//         if (!root) return 0;
+//         return root->val + getsum(root->left) + getsum(root->right);
+//     }
+
+//     ll dfs(TreeNode* root) {
+//         if (!root) return 0;
+//         ll curr = root->val + dfs(root->left) + dfs(root->right);
+//         ans = max(ans, curr * (tot_sum - curr));
+//         return curr;
+//     }
+
+// public:
+//     int maxProduct(TreeNode* root) {
+//         tot_sum = getsum(root);
+//         dfs(root);
+//         return ans % mod;
+//     }
+// };
+
+
+//  by using map , still we can solve this pb by well 
+
  class Solution {
-    
+
+    unordered_map<TreeNode*,int> mp;
     #define ll long long
     int mod = 1e9 + 7;
     ll tot_sum = 0, ans = 0;
-
+    
     ll getsum(TreeNode* root) {
         if (!root) return 0;
-        return root->val + getsum(root->left) + getsum(root->right);
+        if(mp.count(root)) return mp[root];
+        return mp[root]  =  root->val + getsum(root->left) + getsum(root->right);
     }
 
     ll dfs(TreeNode* root) {
