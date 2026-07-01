5ad708db75526d6d64fca48b1e65feb817c2479d
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-07-01 23:51:49 +0530

    Time: 2 ms (88.66%) | Memory: 25.8 MB (44.33%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         vector<int> result(n);
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                result[i] = 0;
-            } else {
-                int steps = nums[i];
-                int newIndex = (i + steps) % n;
-                if (newIndex < 0) newIndex += n;
-                result[i] = nums[newIndex];
-            }
+        for(int i=0;i<n;i++){
+           
+           int shift = nums[i];
+
+           result[i] = nums[(( i + shift ) % n + n ) % n]; // this handle all +ve,-ve
+
+
+ 
         }
+
         return result;
     }
 };
