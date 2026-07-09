8dd8e6e2163676d7aa01fd5059e147bbd71ca15a
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-07-09 11:14:58 +0530

    Time: 53 ms (47.03%) | Memory: 160.7 MB (41.33%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,55 +1,53 @@
 class Solution {
+    #define ll long long 
     int mod = 1e9 + 7;
-#define ll long long
 public:
-    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& qry) {
+        
         int n = s.size();
-        vector<ll> non_zero_count(n, 0);
-        vector<ll> num_upto(n, 0);
-        vector<ll> digit_sum(n, 0);
-        vector<ll> pow10(n + 1, 0);
 
+        vector<ll> dig_sum(n,0);
+        vector<ll> build_num(n,0);
+        vector<ll> dig_count(n,0);
+        vector<ll> pow10(n,0);
         pow10[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            pow10[i] = (pow10[i - 1] * 10) % mod;
+        for(int i=1;i<n;i++){
+            pow10[i] = (pow10[i-1] * 10) % mod;
         }
-        non_zero_count[0] = ((s[0] - '0') != 0) ? 1 : 0;
-
-        for (int i = 1; i < n; i++) {
-            int digit = s[i] - '0';
-            non_zero_count[i] = non_zero_count[i - 1] + ((digit != 0) ? 1 : 0);
+        dig_count[0] = (s[0]-'0' == 0) ?0 : 1;
+        dig_sum[0] = (s[0]-'0');
+         for(int i=1;i<n;i++){
+            int dig = s[i]-'0';
+            if(dig != 0)dig_count[i] = dig_count[i-1] + 1;
+            else dig_count[i] = dig_count[i-1];
+            // dig sum 
+            dig_sum[i] = (dig_sum[i-1] + dig) % mod;
         }
-
-        num_upto[0] = (s[0] - '0');
-        for (int i = 1; i < n; i++) {
-            int digit = s[i] - '0';
-
-            if (digit != 0)
-                num_upto[i] = (num_upto[i - 1] * 10 + digit) % mod;
-            else
-                num_upto[i] = num_upto[i - 1];
+        // build num;
+        build_num[0] = (s[0]-'0');
+        for(int i=1;i<n;i++){
+            int dig = s[i]-'0';
+            if(dig != 0){
+                build_num[i] = ((build_num[i-1] * 10) % mod + dig) % mod;
+            }else build_num[i] = build_num[i-1];
         }
+            int q = qry.size();
+            vector<int> ans(q,0);
+            for(int i=0;i<q;i++){
+                int l = qry[i][0],r = qry[i][1];
 
-        digit_sum[0] = (s[0] - '0');
-        for (int i = 1; i < n; i++) {
-            int digit = s[i] - '0';
-            digit_sum[i] = digit_sum[i - 1] + digit;
-        }
+                ll sum = dig_sum[r] - ( (l==0) ? 0 : dig_sum[l-1]);
+                int k = dig_count[r] - ( (l==0) ? 0 : dig_count[l-1]);
 
-        int q = queries.size();
-        vector<int> ans(q);
-        for (int i = 0; i < q; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-           int num_b4 = (l == 0) ? 0 : num_upto[l-1];
-            int k = non_zero_count[r] - ((l == 0) ? 0 : non_zero_count[l - 1]);
-            ll sum = digit_sum[r] - (l == 0 ? 0 : digit_sum[l - 1]);
-           ll x = (num_upto[r] - (num_b4 * pow10[k]) % mod + mod) % mod;
+                ll left_num = 0;
+                if(l){
+                  left_num =  ( build_num[l-1] * pow10[k] ) % mod;
+                }
+                ll num =( build_num[r] - left_num + mod ) % mod;
 
-            ans[i] = (x * (sum % mod)) % mod;
-        }
+                ans[i] = (sum * num ) % mod;
+            }
 
-        return ans;
+            return ans;
     }
 };
