2ebbf7e50c3131aae9b8ae588568e5f3a54db9bb
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-07-26 09:20:27 +0530

    Time: 35 ms (8.34%) | Memory: 24 MB (5.78%) - LeetSync

--- 4343-create-grid-with-exactly-k-paths-i/create-grid-with-exactly-k-paths-i.cpp
+++ 4343-create-grid-with-exactly-k-paths-i/create-grid-with-exactly-k-paths-i.cpp
@@ -1,107 +1,171 @@
-//  for this pb, simple build the manually think grid  , that have exact k path // rather than building the backtracking o(r dp to count the path it lead to exponentially time complexity) 
+// //  for this pb, simple build the manually think grid  , that have exact k path // rather than building the backtracking o(r dp to count the path it lead to exponentially time complexity) 
 
-class Solution{
-    vector<vector<char>> grid;
-    vector<string> ans;
-    int m,n,k;
-    int found = 0;
+// class Solution{
+//     vector<vector<char>> grid;
+//     vector<string> ans;
+//     int m,n,k;
+//     int found = 0;
 
-    int r = 0,c = 0;
+//     int r = 0,c = 0;
 
-    void solve(){
+//     void solve(){
 
-        if(k == 1){
-            found = 1;
-            r = 0,c = 0;
-        }else if(k == 2){
-            if(m >=2 && n >= 2){
-                found = 1;
-                r = 1,c = 1;
-            }
+//         if(k == 1){
+//             found = 1;
+//             r = 0,c = 0;
+//         }else if(k == 2){
+//             if(m >=2 && n >= 2){
+//                 found = 1;
+//                 r = 1,c = 1;
+//             }
 
-        }else if(k == 3){
-            if( m >= 2  && n >= 3){
-                found =1;
-                r = 1,c = 2;
-            }else if(m >= 3 && n>=2){
-                found = 1;
-                r = 2,c = 1;
-            }
+//         }else if(k == 3){
+//             if( m >= 2  && n >= 3){
+//                 found =1;
+//                 r = 1,c = 2;
+//             }else if(m >= 3 && n>=2){
+//                 found = 1;
+//                 r = 2,c = 1;
+//             }
 
-        }else if(k == 4){
-            if(m >= 2 && n >= 4){
-                r = 1,c = 3;
-                found = 1;
-            }else if( m >= 4 && n >= 2){
-                found = 1;
-                r = 3,c = 1;
-            }else if((m >= 3 && n >= 3)){
-                found = 2;
-                r = 2,c = 2;
-            }
-        }
+//         }else if(k == 4){
+//             if(m >= 2 && n >= 4){
+//                 r = 1,c = 3;
+//                 found = 1;
+//             }else if( m >= 4 && n >= 2){
+//                 found = 1;
+//                 r = 3,c = 1;
+//             }else if((m >= 3 && n >= 3)){
+//                 found = 2;
+//                 r = 2,c = 2;
+//             }
+//         }
 
-        if(found){
-                for(int i=0; i < m; i++){
-                    for(int j = 0; j < n; j++){
-                        if(i <=r && j <= c)continue;
-                        grid[i][j] = '#';
+//         if(found){
+//                 for(int i=0; i < m; i++){
+//                     for(int j = 0; j < n; j++){
+//                         if(i <=r && j <= c)continue;
+//                         grid[i][j] = '#';
+//                     }
+
+//                 }
+//         }
+//     }
+
+
+// public:
+//     vector<string> createGrid(int mm, int nn, int kk) {
+//         m = mm,n = nn,k=kk;
+        
+//         ans.clear();
+//         grid.assign(m,vector<char>(n,'.'));
+        
+//         found = 0;
+//         solve(); // lets find the center of min cell got exact k path, and possibility to make path or not
+
+//         if(m >=3 && n >= 3 && r == 2 && c == 2){
+//                 grid[0][2] = '#',grid[2][0] = '#';
+//         }
+
+//         if(found){
+//             // first mark  3 y 3 , bc
+        
+//             if(r+1 < m ){  // it means first go bottom, then right
+//                 for(int i = r;i < m; i++){
+//                     grid[i][c] = '.';
+//                 }
+//                 for(int j = c;j < n; j++){
+//                     grid[m-1][j] = '.';
+//                 }
+//             } else if(c+1 < n) { // it means first go right , then bottom
+//                 for(int j = c;j < n; j++){
+//                     grid[r][j] = '.';
+//                 }
+//                 for(int i = r;i < m; i++){
+//                     grid[i][n-1] = '.';
+//                 }
+               
+//             }
+
+//         }
+
+//         if(found){
+//                 for(int i=0; i < m; i++){
+//                         string s = "";
+//                         for(int j = 0; j < n; j++){
+//                            s += grid[i][j];
+//                         }
+//                         ans.push_back(s);
+
+//                 }
+//         }
+
+//         return ans;
+
+//     }
+// };
+
+
+class Solution {
+public:
+    vector<vector<int>> paths(int m, int n, vector<string>& grid) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != '#') {
+                    if (i != 0 && j != 0)
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    else {
+                        if (i == 0 && j != 0)
+                            dp[i][j] = dp[i][j - 1];
+                        if (i != 0 && j == 0)
+                            dp[i][j] = dp[i - 1][j];
                     }
-
                 }
+            }
         }
+        return dp;
     }
 
+    bool backtrack(vector<string>& grid, int m, int n, int idx, int k) {
+        auto dp = paths(m, n, grid);
+        int total = dp[m - 1][n - 1];
 
-public:
-    vector<string> createGrid(int mm, int nn, int kk) {
-        m = mm,n = nn,k=kk;
-        
-        ans.clear();
-        grid.assign(m,vector<char>(n,'.'));
-        
-        found = 0;
-        solve();
+        if (total == k)
+            return true;
 
-        if(m >=3 && n >= 3 && r == 2 && c == 2){
-                grid[0][2] = '#',grid[2][0] = '#';
-        }
+        if (total < k)
+            return false;
 
-        if(found){
-            // first mark  3 y 3 , bc
-            
+        if (idx == m * n)
+            return false;
 
-            if(r+1 < m ){  // it means first go bottom, then right
-                for(int i = r;i < m; i++){
-                    grid[i][c] = '.';
-                }
-                for(int j = c;j < n; j++){
-                    grid[m-1][j] = '.';
-                }
-            } else if(c+1 < n) { // it means first go right , then bottom
-                for(int j = c;j < n; j++){
-                    grid[r][j] = '.';
-                }
-                for(int i = r;i < m; i++){
-                    grid[i][n-1] = '.';
-                }
-               
-            }
+        int row = idx / n, col = idx % n;
 
-        }
+        if ((row == 0 && col == 0) || (row == m - 1 && col == n - 1))
+            return backtrack(grid, m, n, idx + 1, k);
 
-        if(found){
-                 for(int i=0; i < m; i++){
-                    string s = "";
-                        for(int j = 0; j < n; j++){
-                           s += grid[i][j];
-                        }
-                        ans.push_back(s);
+        // place
+        grid[row][col] = '#';
+        if (backtrack(grid, m, n, idx + 1, k))
+            return true;
+        // backtrack
+        grid[row][col] = '.';
 
-                    }
-        }
+        // do not place
+        if (backtrack(grid, m, n, idx + 1, k))
+            return true;
 
-        return ans;
+        return false;
+    }
 
+    vector<string> createGrid(int m, int n, int k) {
+        vector<string> grid(m, string(n, '.'));
+        if (backtrack(grid, m, n, 0, k))
+            return grid;
+        return {};
     }
 };
+    
