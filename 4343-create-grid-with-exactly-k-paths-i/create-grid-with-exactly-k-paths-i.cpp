6c1b1d06372340c7708e37c0356880112c7d0508
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-07-26 08:55:46 +0530

    Time: 3 ms (34.76%) | Memory: 10.1 MB (30.73%) - LeetSync

---
+++ 4343-create-grid-with-exactly-k-paths-i/create-grid-with-exactly-k-paths-i.cpp
@@ -0,0 +1,135 @@
+class Solution {
+    const int static N = 12;
+ 
+    vector<vector<char>> grid;
+    vector<string> ans;
+    int m,n,k;
+    int found = 0;
+
+    int r = 0,c = 0;
+
+    void solve(){
+
+        if(k == 1){
+            found = 1;
+            for(int i=0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(i == 0 || j== n-1)continue;
+                    grid[i][j] = '#';
+                }
+            }
+        }else if(k == 2){
+
+            if(m >=2 && n >= 2){
+                found = 1;
+                
+                r = 1,c = 1;
+            }
+            if(found){
+                for(int i=0; i < m; i++){
+                     for(int j = 0; j < n; j++){
+                        if(i <=r && j <= c)continue;
+                        grid[i][j] = '#';
+                    }
+
+                }
+            }
+
+        }else if(k == 3){
+
+            if( m >= 2  && n >= 3){
+                found =1;
+                r = 1,c = 2;
+
+            }else if(m >= 3 && n>=2){
+                found = 1;
+                r = 2,c = 1;
+            }
+
+            if(found){
+                    for(int i=0; i < m; i++){
+                     for(int j = 0; j < n; j++){
+                        if(i <= r && j <=  c)continue;
+                        grid[i][j] = '#';
+                    }
+
+                }
+            }
+
+
+        }else if(k == 4){
+            if(m >= 2 && n >= 4){
+                r = 1,c = 3;
+                found = 1;
+            }else if( m >= 4 && n >= 2){
+                found = 1;
+                r = 3,c = 1;
+            }else if((m >= 3 && n >= 3)){
+                found = 2;
+                r = 2,c = 2;
+            }
+
+            if(found){
+                    for(int i=0; i < m; i++){
+                        for(int j = 0; j < n; j++){
+                            if(i <= r && j <=  c)continue;
+                            grid[i][j] = '#';
+                        }
+
+                    }
+            }
+        }
+    }
+
+
+public:
+    vector<string> createGrid(int mm, int nn, int kk) {
+        m = mm,n = nn,k=kk;
+        
+        ans.clear();
+        grid.assign(m,vector<char>(n,'.'));
+        
+        found = 0;
+        solve();
+
+        if(found && k>=2){
+            // first mark  3 y 3 , bc
+            if(m >=3 && n >= 3 && r == 2 && c == 2){
+                grid[0][2] = '#',grid[2][0] = '#';
+            }
+
+            if(r+1 < m ){  // it means first go bottom, then right
+                for(int i = r;i < m; i++){
+                    grid[i][c] = '.';
+                }
+                for(int j = c;j < n; j++){
+                    grid[m-1][j] = '.';
+                }
+            } else if(c+1 < n) { // it means first go right , then bottom
+                for(int j = c;j < n; j++){
+                    grid[r][j] = '.';
+                }
+                for(int i = r;i < m; i++){
+                    grid[i][n-1] = '.';
+                }
+               
+            }
+
+        }
+
+        if(found){
+            cout<<r<<" "<<c;
+                 for(int i=0; i < m; i++){
+                    string s = "";
+                        for(int j = 0; j < n; j++){
+                           s += grid[i][j];
+                        }
+                        ans.push_back(s);
+
+                    }
+        }
+
+        return ans;
+
+    }
+};
