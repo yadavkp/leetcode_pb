eb83e95716442058101d559dc8a0adb9feb117f4
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-08-18 23:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (88.72%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -8,48 +8,24 @@ public:
              return *max_element(nums.begin(),nums.end());
         }
 
+        vector<int> frq(51,0);
+        for(int v : nums)frq[v]++;
+
         if(k == 1){
-            
-            vector<int> frq(51,0);
-            for(int v : nums)frq[v]++;
             for(int i = 50;i>=0;i--){
                 if(frq[i]==1){
                     return i;
                 }
             }
             return -1;
-
         }
 
-
-        int first  = nums[0], last = nums[n-1];
-
-        if(first == last) return -1;
-
-        int mx = 0;
-        if(first > last)mx = first;
-        else {
-            if(last > first){
-                mx = last;
-                last = first;
-            }
-        }
-
-        int cnt = 0;
-        for(int v : nums){
-            if(v == mx)cnt++;
-        }
-
-        if(cnt == 1)return mx;
-        cnt = 0;
-        for(int v : nums){
-            if(v == last)cnt++;
-        }
-
-        if(cnt == 1)return last;
-
-        return -1;
-
+        if(nums[0] == nums[n-1]) return -1;
+        int ans = -1;
+        if(frq[nums[0]]== 1 && frq[nums[n-1]]==1)ans = max(nums[0],nums[n-1]);
+        else if( frq[nums[n-1]]==1)ans = nums[n-1];
+        else if(frq[nums[0]]==1)ans = nums[0];
         
+       return ans;
     }
 };
