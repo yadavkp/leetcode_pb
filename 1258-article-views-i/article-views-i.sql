c5a6202474d7954254cc262b206bcf882cab72b4
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-08-19 23:43:55 +0530

    Time: 684 ms (6.01%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -6,15 +6,11 @@
 -- order by author_id asc;
 
 
+
 select distinct v2.viewer_id as id
 from Views as v1
 join Views as v2
 on v1.viewer_id = v2.viewer_id
-where v1.author_id = v2.viewer_id
+and v1.author_id = v2.viewer_id
 order by id asc;
 
-
--- select v1.article_id,v1.viewer_id,v2.author_id
--- from Views as v1
---  join Views as v2
--- on v1.article_id = v2.viewer_id;
