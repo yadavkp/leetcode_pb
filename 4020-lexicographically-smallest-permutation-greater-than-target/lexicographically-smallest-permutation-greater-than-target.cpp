c06db10b0e2f6c1703a0cd3522d539547964cd9e
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-08-28 12:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (67.86%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,70 @@
+class Solution {
+    #define pb push_back
+    vector<int> frq;
+    string ans = "";
+    void dfs(int i,int f,string &ss,string &tar){
+        
+        if(i >= tar.size()){
+            if(f == 1){
+                if(ans.empty()) ans = ss;
+                
+            }
+            return ;
+        }
+
+
+        for(int k = 0;k  < 26; k++){
+
+           
+            if(frq[k] > 0){
+
+                char ch = k + 'a';
+
+                 if(ch == tar[i]){
+                   ss += ch;
+                   frq[ch-'a']--;
+                    dfs(i+1,0,ss,tar);
+                    frq[ch-'a']++;
+                    ss.pop_back();
+
+
+                    if(!ans.empty()) return ;
+                    
+                }else if(ch > tar[i]){
+
+                    ss += ch;
+                    frq[ch-'a']--;
+                   
+                    for(int l = 0;l < 26;l++){
+                            while(frq[l]>0){
+                                ss += (l + 'a');
+                                frq[l]--;
+                            }
+                    }
+                    ans = ss;
+                    
+                    return;
+
+                }
+                
+            }
+        }
+
+
+    }
+public:
+    string lexGreaterPermutation(string s, string target) {
+        
+        frq.assign(26,0);
+        ans = "";
+        for(char ch : s){
+            frq[ch -'a']++;
+        }
+
+        string ss = "";
+        
+        dfs(0,0,ss,target);
+
+        return ans;
+    }
+};
