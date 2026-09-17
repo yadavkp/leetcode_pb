4a9945563bf649d08754f18fe9e414dfbbd1ded4
yadavkp <163226725+yadavkp@users.noreply.github.com>
2026-09-17 18:33:14 +0530

    Time: 692 ms (27.43%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -6,11 +6,13 @@ select
     e1.department_id
 from Employee as e1
 where (
-    select count(*)
+    select count(inner_q.employee_id)
     from Employee as inner_q
     where inner_q.employee_id = e1.employee_id
 ) = 1
 or e1.primary_flag = "Y";
+
+
 
 
 
